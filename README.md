fa3b6cad9d5d98f13648129c3cb5f685c054b9a8
wesbdss <wesleybenicio4@gmail.com>
2021-05-12 23:21:16 -0300

    Arquivos kafka configurados

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kafkaJs-test
+O kafka tem que ser configurado de acordo com o ip da máquina,  e as configurações estão no docker-compose
+
+Depois apenas rodar o consumer e o producer
+
+Há ainda algumas dúvidas sobre o processo:
+- Como retornar uma resposta de conclusão de volta para usuário, visto que parece ser unidirecional
+- Como fechar o acesso a outros usuário do kafka em rede
+
---
+++ example/consumer.js
@@ -0,0 +1,55 @@
+const ip = require('ip')
+const {Kafka, logLevel} = require('kafkajs')
+const host = process.env.HOST_IP || ip.address()
+
+const kafka = new Kafka( {
+    logLevel: logLevel.INFO,
+    brokers: [`${host}:9092`],
+    clientId: 'example-consumer'
+})
+
+const topic = 'topic-test'
+const consumer = kafka.consumer({groupId: 'test-group'})
+
+const run = async () => {
+    await consumer.connect()
+    await consumer.subscribe({topic, fromBeginning: true})
+    await consumer. run( {
+        eachMessage: async ({topic, partition, message}) =>{
+            const prefix = `${topic}[${partition} | ${message.offset}] /${message.timestamp}`
+            console.log(`- ${prefix} ${message.key}#${message.value}`)
+
+            console.log(`[Message] => ${message}`)
+        }
+    })
+}
+
+run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
+
+
+//  Parte de Tratamento de Errors
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.map(type => {
+    process.on(type, async e => {
+      try {
+        console.log(`process.on ${type}`)
+        console.error(e)
+        await consumer.disconnect()
+        process.exit(0)
+      } catch (_) {
+        process.exit(1)
+      }
+    })
+  })
+  
+  signalTraps.map(type => {
+    process.once(type, async () => {
+      try {
+        await consumer.disconnect()
+      } finally {
+        process.kill(process.pid, type)
+      }
+    })
+  })
